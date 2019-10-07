53a82356141c78cd03d1c9b0181aed00eaeff88a
yanagiragi <qaz90473@gmail.com>
2019-10-07 09:02:40 +0800

    fix login error

--- pttLogin.js
+++ pttLogin.js
@@ -20,20 +20,27 @@ function loginDataHandler (newdataStr, id, ps) {
 		g_conn.write('G\r')
 		g_conn.write('Y\r')
 		process.exit()
-	} else if (newdataStr.indexOf('您想刪除其他重複登入的連線嗎') != -1) {
+	}
+	else if (newdataStr.indexOf('您想刪除其他重複登入的連線嗎') != -1) {
 		g_conn.write('n\r')
 		// console.log( '已刪除其他重複登入的連線' );
-	} else if (newdataStr.indexOf('登入中') != -1) {
+	}
+	else if (newdataStr.indexOf('登入中') != -1) {
 		console.log('登入中...')
-	} else if (newdataStr.indexOf('請輸入您的密碼') != -1) {
+	}
+	/*else if (newdataStr.indexOf('請輸入您的密碼') != -1) {
 		console.log('請輸入您的密碼:')
 		g_conn.write(ps + '\r')
-		console.log('(已輸入密碼)')
-	} else if (newdataStr.indexOf('請輸入代號，或以 guest 參觀，或以 new 註冊:') != -1) {
+		console.log('(已輸入密碼)' + ps)
+	} */
+	else if (newdataStr.indexOf('請輸入代號，或以 guest 參觀，或以 new 註冊:') != -1) {
 		console.log('請輸入代號，或以 guest 參觀，或以 new 註冊:')
 		g_conn.write(id + '\r')
+		g_conn.write(ps + '\r')
 		console.log('(已輸入帳號)')
-	} else {
+	}
+	else {
+		console.log(newdataStr)
 	}
 }
 
@@ -56,7 +63,8 @@ function login (id, ps) {
 
 	g_conn.addListener('timeout', function () {
 		// Emitted if the socket times out from inactivity. This is only to notify that the socket has been idle. The user must manually close the connection.
-		loginDataHandler(g_data, id, ps)
+		// wait 1 sec then preform action
+		setTimeout(() => {loginDataHandler(g_data, id, ps)}, 1000)
 	})
 
 	return g_conn
